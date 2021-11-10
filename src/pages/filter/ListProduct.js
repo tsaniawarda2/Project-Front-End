6a284022837d6a2a6c3649ba86482a017f2759a4
tsaniawarda2 <63621268+tsaniawarda2@users.noreply.github.com>
2021-11-10 20:47:52 +0700

    update inStock

--- src/pages/filter/ListProduct.js
+++ src/pages/filter/ListProduct.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "../../assets/css/Catalogs.css";
+import Sold from "../../assets/image/sold.png";
 
 export default function ListProduct(props) {
   const { data } = props;
@@ -11,15 +13,34 @@ export default function ListProduct(props) {
           <>
             <div className="col-md-4 gx-3 gy-5">
               <div className="card h-100 text-center" key={idx}>
-                <img
-                  className="card-img-top"
-                  src={product.image}
-                  alt={product.name}
-                />
+                <div className="image-card">
+                  <img
+                    className="card-img-top"
+                    src={product.image}
+                    alt={product.name}
+                  />
+
+                  {product.inStock ? (
+                    ""
+                  ) : (
+                    <img className="sold" src={Sold} alt="Sold" />
+                  )}
+                </div>
+
                 <div className="card-body">
                   <h5 className="card-title">{product.name}</h5>
                   <p className="card-text price">IDR {product.price}</p>
-                  <p className="card-text year">{product.year}</p>
+                  <p className="card-text condition">{product.condition}</p>
+                  <button
+                    className="btn btn-primary"
+                    disabled={!product.inStock}
+                  >
+                    {product.inStock ? (
+                      <div>Add To Car</div>
+                    ) : (
+                      <div>Out of Stock</div>
+                    )}
+                  </button>
                 </div>
               </div>
             </div>
